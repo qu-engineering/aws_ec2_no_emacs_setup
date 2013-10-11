0110d2eda81ec2ae6f57431f8d2daa784f76816b
Munair Simpson <munair@gmail.com>
2013-10-11 16:18:07 +0900

    try to remove interactivity of this script
    added -y option to add-apt-repository for node.js

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 # Install a special package
 sudo apt-get install -y python-software-properties python g++ make 
 # Add a new repository for apt-get to search
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository -y ppa:chris-lea/node.js
 # Update apt-get’s knowledge of which packages are where
 sudo apt-get update
 # Now install nodejs and npm
